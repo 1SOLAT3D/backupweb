c70486e70c0e7add7c7b3f6c260b24f78006830c
Franco Martinez Cisneros <franco_dortmund13@live.com.mx>
2021-09-13 10:25:48 -0500

    Archivo Java

---
+++ JavaProjects/Ejemplo1/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a =1;
+        int b =2;
+        int c =10;
+        int suma =0;
+
+        System.out.println(a);
+        System.out.println(b);
+        int x = 1;
+        while (x<=c){
+            suma = a+b;
+            System.out.println(suma);
+            a = b;
+            b = suma;
+            x++;
+        }
+    }
+}
